c91720f4319c90ab7d30704ae5ebf48222b03fbe
Timothy Ross Grey <TimGrey@Timothys-MacBook-Pro-7.local>
2019-08-20 10:03:24 -0700

    Added more code snippets

--- Flexbox-Pattern-Website/index.js
+++ Flexbox-Pattern-Website/index.js
@@ -2,3 +2,20 @@
 function myFunction(x) {
     x.classList.toggle("fa-thumbs-down");
   }
+
+  // TOGGLE LIST10 ========= BOX 10=========BOX 10=========BOX 
+
+  function logItem(e) {
+    const item = document.querySelector(`[data-id=${e.target.id}]`);
+    item.toggleAttribute('hidden');
+  }
+  
+  const chapters = document.querySelectorAll('details');
+  chapters.forEach((chapter) => {
+    chapter.addEventListener('toggle', logItem);
+  });
+
+
+  // BOX 11=========BOX 11=========BOX 11=========BOX 11=========
+
+ 
